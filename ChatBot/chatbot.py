1ad584ba39a4b9b785cfb7e539882f6cbc3ad7f3
Timalchuk Diana <d.i.timalchuk@student.khai.edu>
2022-12-15 18:31:38 +0200

    ChatBot 2-nd stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -2,3 +2,6 @@ bot_name = "Airs"
 birth_year = "2022"
 print("Hello! My name is,", bot_name, "\nI was created in ", birth_year, )
 
+print("Please, remind me your name")
+user_name = input()
+print("What a great name you have,", user_name, "!")
