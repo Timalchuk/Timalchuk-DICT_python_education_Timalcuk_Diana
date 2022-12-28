a28c9e1b1701555d5b101e7348fc95e6995a2b3e
Timalchuk Diana <d.i.timalchuk@student.khai.edu>
2022-12-28 22:25:25 +0200

    Hangman stage 1
    hangman.py
    
    hangman.py
    
    hangman.py

---
+++ hangman/hangman.py
@@ -0,0 +1,48 @@
+""" Python: practice № 5 'Hangman' """
+import random
+
+
+print("HANGMAN")
+print("The game will be available soon.")
+
+word = ("python", "hello", "cat", "java", "javascript")
+take = random.choice(word)
+
+print('there are', len(take), ' letters in a word')
+
+guessed = ['_'] * len(take)
+wrong = []
+
+
+def transformation():
+    for x in guessed:
+        print(x, end=' ')
+    print()
+
+
+transformation()
+
+while True:
+
+    user_write = input("Input a letter:")
+    if user_write in take:
+        index = 0
+        for i in take:
+            if i == user_write:
+                guessed[index] = user_write
+            index += 1
+        transformation()
+    else:
+             if user_write not in wrong:
+               wrong.append(user_write)
+             else:
+                print('You already guessed that')
+             print(wrong)
+    if len(wrong) > 6:
+
+        print('You lost!')
+        print("Wrong", take)
+        break
+    if '_' not in guessed:
+        print('You win!')
+        break
